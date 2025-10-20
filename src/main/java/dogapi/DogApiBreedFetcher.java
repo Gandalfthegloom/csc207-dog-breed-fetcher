455b69882f091914b5bbb24fd9eceb60e1cf06fc
gandalfthegloom <farrellarifandipurbasidadolog@gmail.com>
2025-10-20 01:02:23 -0400

    completed DogApiBreedFetcher

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -3,6 +3,7 @@ package dogapi;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -19,6 +20,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     *
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
@@ -30,6 +32,32 @@ public class DogApiBreedFetcher implements BreedFetcher {
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        String urlMain = "https://dog.ceo/api/breed/" + breed + "/list";
+        Request request = new Request.Builder()
+                .url(urlMain)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            ResponseBody body =  response.body();
+            if (body != null && !response.isSuccessful()) {
+                String json = body.string();
+                JSONObject jsonObject = new JSONObject(json);
+                JSONArray jsonArray = jsonObject.getJSONArray("message");
+
+                ArrayList<String> subBreeds = new ArrayList<>(jsonArray.length());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    subBreeds.add(jsonArray.getString(i));
+                }
+                return subBreeds;
+            }
+
+            throw new BreedNotFoundException("Failed to fetch sub-breeds for '" + breed +
+                    "'. HTTP " + response.code());
+
+        }
+
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
